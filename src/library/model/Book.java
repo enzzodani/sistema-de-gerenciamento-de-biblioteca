0a28e50b2cdb10394e787d0de43790c094827d8b
enzzodani <gomes.enzodani@gmail.com>
2023-11-20 10:45:34 -0300

    Initial attributes (Book and User)

--- src/library/model/Book.java
+++ src/library/model/Book.java
@@ -1,4 +1,16 @@
 package library.model;
 
 public class Book {
+//    Attributes
+    private final String title;
+    private final String author;
+    private final String isbn;
+    private final boolean availability;
+
+    public Book(String title, String author, String isbn, boolean availability) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+        this.availability = availability;
+    }
 }
--- src/library/model/User.java
+++ src/library/model/User.java
@@ -1,4 +1,9 @@
 package library.model;
 
 public class User {
+//    Attributes
+    private String name;
+    private int id;
+
+
 }
