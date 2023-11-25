e84733c1f31342261a9a6f0e00e447dfd5f96506
enzzodani <gomes.enzodani@gmail.com>
2023-11-25 17:11:07 -0300

    update main

--- src/library/app/Main.java
+++ src/library/app/Main.java
@@ -1,4 +1,12 @@
 package library.app;
 
+import library.services.Library;
+
+import java.util.ArrayList;
+
 public class Main {
+    public static void main(String[] args) {
+//        Initialization logical and execution
+        Library library = new Library(new ArrayList<>(), new ArrayList<>(), new ArrayList<>());
+    }
 }
