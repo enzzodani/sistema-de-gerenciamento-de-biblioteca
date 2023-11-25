1fd29c93586546362487aaee4caf19e9a4561a37
enzzodani <gomes.enzodani@gmail.com>
2023-11-25 16:45:01 -0300

    Update User and Loan, add constructor, getters, setters, toString and specifics methods

--- src/library/model/Loan.java
+++ src/library/model/Loan.java
@@ -1,4 +1,17 @@
 package library.model;
 
+import java.time.LocalDate;
+
 public class Loan {
+    private Book book;
+    private User user;
+    private LocalDate startDate;
+    private LocalDate dueDate;
+
+    public Loan(Book book, User user, LocalDate startDate, LocalDate dueDate) {
+        this.book = book;
+        this.user = user;
+        this.startDate = startDate;
+        this.dueDate = dueDate;
+    }
 }
--- src/library/model/User.java
+++ src/library/model/User.java
@@ -1,9 +1,61 @@
 package library.model;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public class User {
 //    Attributes
     private String name;
     private int id;
 
+    private List<Book> borrowedBooks;
 
+//    Constructor
+    public User(String name, int id, List<Book> borrowedBooks) {
+        this.name = name;
+        this.id = id;
+        this.borrowedBooks = borrowedBooks;
+    }
+//    Methods
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public List<Book> getBorrowedBooks() {
+        return borrowedBooks;
+    }
+
+    public void setBorrowedBooks(List<Book> borrowedBooks) {
+        this.borrowedBooks = borrowedBooks;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", id=" + id +
+                ", borrowedBooks=" + borrowedBooks +
+                '}';
+    }
+//    Loan Method
+    public void borrowBook (Book book, LocalDate dueDate) {
+//        Create a Loan Object
+        Loan loan = new Loan(book, this, LocalDate.now(), dueDate);
+
+//        Add book to list
+        borrowedBooks.add(book);
+    }
 }
